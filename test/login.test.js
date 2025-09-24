8113cf6c33c88eab34700e132c7ad824414b2723
Bruna <brunameiraf@gmail.com>
2025-09-24 07:17:16 -0300

    Testes para get transferencia total e individual

--- test/login.test.js
+++ test/login.test.js
@@ -1,18 +1,19 @@
 const request = require('supertest');
-const { expect } = require('chai');
-require('dotenv').config()
+const { expect } = require('chai')
+require('dotenv').config() 
 const postLogin = require('../fixtures/postLogin.json')
 
 describe('Login', () => {
     describe('POST /login', () => {
-        it('Deve retornar 200 com token em string quando utilizar credencias válidas', async() => {
-            const bodyLogin = { ...postLogin}
+        it('Deve retornar 200 com um token em string quando usar credenciais válidas', async () => {
+            const bodyLogin = { ...postLogin }
             const resposta = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type', 'application/json')
                 .send(bodyLogin)
+            
             expect(resposta.status).to.equal(200);
             expect(resposta.body.token).to.be.a('string');
         })
     })
-});
+})
--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -5,24 +5,26 @@ const { obterToken } = require('../helpers/autenticacao')
 const postTranferencias = require('../fixtures/postTransferencias.json')
 
 describe('Transferencias', () => {
+    let token
+
+    beforeEach(async () => {
+        token = await obterToken('julio.lima', '123456')
+    })
+
     describe('POST /transferencias', () => {
-        let token;
-        beforeEach(async () => {
-            token = await obterToken('julio.lima', '123456')
-        })
+
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$10,00', async () => {
-            const bodyTransferencias = {...postTranferencias}
+            const bodyTransferencias = { ...postTranferencias }
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', 'Bearer ' + token)
                 .send(bodyTransferencias)
             expect(resposta.status).to.equal(201);
-            console.log(resposta.body)
         })
 
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de R$10,00', async () => {
-            const bodyTransferencias = {...postTranferencias}
+            const bodyTransferencias = { ...postTranferencias }
             bodyTransferencias.valor = 7;
             const resposta = await request('http://localhost:3000')
                 .post('/transferencias')
@@ -30,7 +32,32 @@ describe('Transferencias', () => {
                 .set('Authorization', 'Bearer ' + token)
                 .send(bodyTransferencias)
             expect(resposta.status).to.equal(422);
-            console.log(resposta.body)
+        })
+    })
+
+    describe('GET /transferencias/{id}', () => {
+        it('Deve retornar sucesso com 200 e dados iguais ao registro do banco de dados quando o registro for válido', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias/1')
+                .set('Authorization', `Bearer ${token}`)
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.id).to.equal(1)
+            expect(resposta.body.id).to.be.a('number')
+            expect(resposta.body.conta_origem_id).to.equal(1)
+            expect(resposta.body.conta_destino_id).to.equal(2)
+            expect(resposta.body.valor).to.equal(11.00)
+        })
+
+    })
+    describe('GET /transferencias', () => {
+        it('Deve retronar 10 elementos na paginação quando informar limite de 10 registros', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias?page=1&limit=10')
+                .set('Authorization', `Bearer ${token}`)
+                expect(resposta.status).to.equal(200)
+                expect(resposta.body.limit).to.equal(10)
+                expect(resposta.body.transferencias).to.have.lengthOf(10)
+                console.log(resposta.body)
         })
     })
 })
